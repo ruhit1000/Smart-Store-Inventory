a821377197ce8e77cec1fbdd15d6c9267743a252
Ruhit Paul <ruhitpaul18@gmail.com>
2026-03-22 14:04:35 +0600

    navabr, menubar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import MenuBar from "./components/MenuBar/MenuBar"
+import Navbar from "./components/Navbar/Navbar"
 
 
 function App() {
-  
+
 
   return (
-    <>
-      
-    </>
+    <div className="h-screen">
+      <header>
+        <Navbar />
+      </header>
+      <main className="grid grid-cols-5">
+        <div>
+          <MenuBar />
+        </div>
+        <div>
+          
+        </div>
+      </main>
+    </div>
   )
 }
 
